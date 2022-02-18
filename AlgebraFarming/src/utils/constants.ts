a9d36b1fd34ea0a51e37b0499d718c349cea9717
IliaAzhel <aje-ilia2013@yandex.by>
2022-02-18 14:45:45 +0300

    [AlgebraFarming] add liquidity in deposits

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
 
 
-export const FarmingCenterAddress = Address.fromString('0xb9bb1d4202383a25c90115ae9ec2543fa229dfaa')
+export const FarmingCenterAddress = Address.fromString('0xcb8c37b241c62ee3b043608667db102a91f22ae9')
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
