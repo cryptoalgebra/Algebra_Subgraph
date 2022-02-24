7b738d9a819b54a492d1df696b50ba1917791a06
IliaAzhel <aje-ilia2013@yandex.by>
2022-02-24 14:36:34 +0300

    [Common] conf for pulse

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
 
 
-export const FarmingCenterAddress = Address.fromString('0xcb8c37b241c62ee3b043608667db102a91f22ae9')
+export const FarmingCenterAddress = Address.fromString('0xc970bfc8341eb670c960a67ea22f18ab77a02c75')
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
