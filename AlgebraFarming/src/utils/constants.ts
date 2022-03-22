6965e2499a5a202f35bcac743641324c87c2467b
IliaAzhel <aje-ilia2013@yandex.by>
2022-03-22 18:48:33 +0300

    [Common] update for oasis

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
 
 
-export const FarmingCenterAddress = Address.fromString('0xb9bb1d4202383a25c90115ae9ec2543fa229dfaa')
+export const FarmingCenterAddress = Address.fromString('0x9ced021721093aA5235DAECBFFF6D2276c88A372')
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
