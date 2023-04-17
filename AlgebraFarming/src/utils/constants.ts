e5b2042f122dc03d57dd7d17866f4831b44e3e6b
IliaAzhel <aje-ilia2013@yandex.by>
2023-04-17 22:06:22 +0300

    init glacier farming subgraph

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,3 +1,3 @@
 import { Address } from '@graphprotocol/graph-ts';
 
-export const FarmingCenterAddress = Address.fromString("0xB1d4784473277c162E36656fa066dfEd21B1EC89")
+export const FarmingCenterAddress = Address.fromString("0x2aBC644E18370627E7F38816e3973199e3B77E11")
