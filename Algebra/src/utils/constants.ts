8f371266d30255c349159f976fe5874e4b3349e2
IliaAzhel <aje-ilia2013@yandex.by>
2023-12-26 12:16:37 +0300

    fusion config

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,9 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xC848bc597903B4200b9427a3d7F61e3FF0553913'
-export const SLUSH_ADDRESS = '0x8309bc8bb43fb54db02da7d8bf87192355532829'
-export const ICE_ADDRESS = Address.fromString('0x005e16eccdfd3ea76e7b777a1beb7b826e3aa7e3')
+export const FACTORY_ADDRESS = '0x5Bcb6Be858d981f57cD209a3e6b1B4dc76646ED0'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
