6fdafb710071f6bf09f48bc582e066da42619a3b
IliaAzhel <aje-ilia2013@yandex.by>
2024-03-01 16:38:50 +0300

    arthera testnet config

--- Algebra/src/utils/pricing.ts
+++ Algebra/src/utils/pricing.ts
@@ -4,13 +4,13 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WMatic_ADDRESS = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
+const WMatic_ADDRESS = '0x5a1750f9cb8a7e98e1fd618922af276493318710'
 const USDC_WMatic_03_POOL = '0xb104f0535a35a69880dab51008756c31d47dbf0f'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // WMATIC
+  '0x5a1750f9cb8a7e98e1fd618922af276493318710', // WMATIC
   '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
   '0x5aefba317baba46eaf98fd6f381d07673bca6467', // USDT 
   '0x49a390a3dfd2d01389f799965f3af5961f87d228'
