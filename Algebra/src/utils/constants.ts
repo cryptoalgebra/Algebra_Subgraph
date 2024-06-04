11d5dbc53db1507e33376965dc478689b0b65e30
fourlen <oborin.2013@mail.ru>
2024-06-04 16:17:23 +0300

    redeploy

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x731aff9ba38d4f888f7ecce820240f0845bb78cb'
+export const FACTORY_ADDRESS = '0x5eee9d8668356f2482a4aac9eb4040529c4b4dee'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
