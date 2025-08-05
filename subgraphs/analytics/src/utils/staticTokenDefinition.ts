b0d8fda23f85833cdfb4968d3abe9c2836fea23d
IliaAzhel <aje-ilia2013@yandex.by>
2025-08-06 01:44:24 +0300

    add static tokens

--- subgraphs/analytics/src/utils/staticTokenDefinition.ts
+++ subgraphs/analytics/src/utils/staticTokenDefinition.ts
@@ -11,10 +11,28 @@ export class StaticTokenDefinition {
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
     const staticDefinitions: Array<StaticTokenDefinition> = [
       {
-        address: Address.fromString('0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'),
-        symbol: 'DGD',
-        name: 'DGD',
-        decimals: BigInt.fromI32(9)
+        address: Address.fromString('0x96b86cce868fcc6681d8ab2c8e53c93e9750ba3a'),
+        symbol: 'PKEY',
+        name: 'PuzzleKey',
+        decimals: BigInt.fromI32(18)
+      },
+      {
+        address: Address.fromString('0xacB8b71500B616c20fE18c48fc965D0981538D54'),
+        symbol: 'GRUMPY',
+        name: 'GRUMPY ON TARA',
+        decimals: BigInt.fromI32(18)
+      },
+      {
+        address: Address.fromString('0x69D411CbF6dBaD54Bfe36f81d0a39922625bC78c'),
+        symbol: 'USDT',
+        name: ' Tether USD',
+        decimals: BigInt.fromI32(6)
+      },
+      {
+        address: Address.fromString('0x9f3f1fa0822463f592c1725ED08a9cF261958627'),
+        symbol: 'WTARA',
+        name: 'Wrapped Taraxa',
+        decimals: BigInt.fromI32(18)
       },
     ]
     return staticDefinitions
