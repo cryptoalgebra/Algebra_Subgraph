69fb51fbe7eba35bce1583543595aaf4c361925d
IliaAzhel <aje-ilia2013@yandex.by>
2025-09-19 18:03:33 +0300

    update algebra base sepolia config

--- config/algebra-base-sepolia/chain.ts
+++ config/algebra-base-sepolia/chain.ts
@@ -2,11 +2,11 @@
 import { BigDecimal} from '@graphprotocol/graph-ts'
 
 // Addresses for analytics subgraph 
-export const FACTORY_ADDRESS = '0xcD58521ecaC7724d1752F941C56490c27bAe9ab0'
-export const NONFUNGIBLE_POSITION_MANAGER_ADDRESS = '0x5baD56bfBABEC1A5A7848399762f54566FA22557'
+export const FACTORY_ADDRESS = '0x2fB84Ae4b1B6aeEc5627268070cF44C678Cd9728'
+export const NONFUNGIBLE_POSITION_MANAGER_ADDRESS = '0x9026d1c84f5834968FE80368b216D7C34109Cf97'
 
 export const REFERENCE_TOKEN = '0x4200000000000000000000000000000000000006' // Wrapped Native Token
-export const STABLE_TOKEN_POOL = '0x6ac1efFa0F55A64D3bFb77a47fF1dA88C0f504D8' // USDC/WETH pool
+export const STABLE_TOKEN_POOL = '0x5500c2FfEBB0B0D03C39F6F39F31000C5ADA3bCA' // USDC/WETH pool
 
 // Minimum reference token locked in pool for pricing calculations
 export const MINIMUM_NATIVE_LOCKED = BigDecimal.fromString('0')
@@ -26,8 +26,8 @@ export const STABLE_COINS: string[] = [
 
 // Addresses for farming subgraph
 // Farming contracts
-export const ETERNAL_FARMING_ADDRESS = '0xf7cA7d0F8Bbef9BBfEB66Cf2c9C84Eeb2dA60b22'  
+export const ETERNAL_FARMING_ADDRESS = '0xc709aCDA0dBF1a70189bd850e8E8b2659017Fa62'  
 
 // Addresses for limit order subgraph
 // Limit order contract
-export const LIMIT_ORDER_ADDRESS = '0x05F9E353559da6f2Bfe9A0980D5C3e84eA5d4238'
+export const LIMIT_ORDER_ADDRESS = '0xe578551955EA80F001DD5C1d1db3F4652a049C5D'
