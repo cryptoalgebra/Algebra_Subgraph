ee28b68a997b6a8494bbd41d9df00479ea354f4a
Lifopam <budakovtfl@gmail.com>
2026-01-20 15:56:03 +0300

    update skale config

--- config/skale-testnet/chain.ts
+++ config/skale-testnet/chain.ts
@@ -5,23 +5,21 @@ import { BigDecimal} from '@graphprotocol/graph-ts'
 export const FACTORY_ADDRESS = '0x99E317c0099F0fB8C5913db976d00fddeDB69583'
 export const NONFUNGIBLE_POSITION_MANAGER_ADDRESS = '0x50FCbF85d23aF7C91f94842FeCd83d16665d27bA'
 
-export const REFERENCE_TOKEN = '0x955B95b8532fe75DDCf2161f61127Be74A768158' // Wrapped Native Token
-export const STABLE_TOKEN_POOL = '0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A' // USDC/WETH pool
+export const REFERENCE_TOKEN = '0x9fad6Fd657ffD9045a1377C692F8Bb2d3d57C7B6' // Wrapped Native Token
+export const STABLE_TOKEN_POOL = '0xe90dfd31c0d8c2adfa996e5ba175bad077a598bf' // USDT/WETH pool
 
 // Minimum reference token locked in pool for pricing calculations
 export const MINIMUM_NATIVE_LOCKED = BigDecimal.fromString('0.001')
 
 // Token lists for tracking volume and liquidity
 export const WHITELIST_TOKENS: string[] = [
-  "0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A", //wnative
-  "0x955B95b8532fe75DDCf2161f61127Be74A768158", // usdc.e
-  "0x325eeb3aa50014f35861e3374f54b3997aa8357d" // usdc
+  "0x9fad6Fd657ffD9045a1377C692F8Bb2d3d57C7B6", // WETH
+  "0x54506e0a71f6c883dA3c7eaDb0aEbEDbaC0c59f5"  // USDT
 ]
 
 // Stable coins for USD pricing (tokens with stable $1 value)
 export const STABLE_COINS: string[] = [
-  '0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A', // usdc.e
-  '0x325eeb3aa50014f35861e3374f54b3997aa8357d' // usdc
+  "0x54506e0a71f6c883dA3c7eaDb0aEbEDbaC0c59f5"  // USDT
 ]
 
 // Addresses for farming subgraph
