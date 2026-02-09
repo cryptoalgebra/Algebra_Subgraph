1f79ad62fa294e028b056214f7d4a9425647921a
IliaAzhel <aje-ilia2013@yandex.by>
2026-02-09 18:49:05 +0300

    update skale config

--- config/skale-testnet/chain.ts
+++ config/skale-testnet/chain.ts
@@ -9,17 +9,17 @@ export const REFERENCE_TOKEN = '0x9fad6Fd657ffD9045a1377C692F8Bb2d3d57C7B6' // W
 export const STABLE_TOKEN_POOL = '0xe90dfd31c0d8c2adfa996e5ba175bad077a598bf' // USDT/WETH pool
 
 // Minimum reference token locked in pool for pricing calculations
-export const MINIMUM_NATIVE_LOCKED = BigDecimal.fromString('0.001')
+export const MINIMUM_NATIVE_LOCKED = BigDecimal.fromString('0.00001')
 
 // Token lists for tracking volume and liquidity
 export const WHITELIST_TOKENS: string[] = [
-  '0x9fad6Fd657ffD9045a1377C692F8Bb2d3d57C7B6', // WETH
-  '0x54506e0a71f6c883dA3c7eaDb0aEbEDbaC0c59f5'  // USDT
+  '0xD2Aaa00700000000000000000000000000000000', // WETH
+  '0x3Ca0A49f511c2c89c4DCbbf1731120d8919050Bf'  // USDT
 ]
 
 // Stable coins for USD pricing (tokens with stable $1 value)
 export const STABLE_COINS: string[] = [
-  '0x54506e0a71f6c883dA3c7eaDb0aEbEDbaC0c59f5'  // USDT
+  '0x3Ca0A49f511c2c89c4DCbbf1731120d8919050Bf'  // USDT
 ]
 
 // Addresses for farming subgraph
@@ -30,4 +30,4 @@ export const ETERNAL_FARMING_ADDRESS = '0x49BE8AA6c684b15e0C5450e8Fa0b16Bec14355
 // Limit order contract
 export const LIMIT_ORDER_ADDRESS = '0x34D6F4f7D27B94C157aE3DB2D17C1beCB1bBF89C'
 
-export const ALM_VAULT_FACTORY_ADDRESS = '0x1B3B62B519a60E8927d4FfbB54681871e1Bb6F11'
+export const ALM_VAULT_FACTORY_ADDRESS = '0x4b36b366f262E5418Ba2D49C3Eb7C546cc48Fa65'
