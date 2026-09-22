057f1a225f9f152c12ea2cf35cf6f1a7841c1f8e
IliaAzhel <aje-ilia2013@yandex.by>
2026-09-22 16:07:26 +0300

    add limit order events

--- subgraphs/limits/src/mappings/limit.ts
+++ subgraphs/limits/src/mappings/limit.ts
@@ -1,11 +1,40 @@
-import { BigInt} from '@graphprotocol/graph-ts';
+import { BigInt, ethereum } from '@graphprotocol/graph-ts';
 import {
     Fill,
     Kill,
     Place,
     Withdraw
 } from '../types/Limit/Limit';
-import { Epoch, LimitOrder } from '../types/schema';
+import { Epoch, EpochAction, LimitOrder, LimitOrderAction } from '../types/schema';
+
+function createLimitOrderAction(event: ethereum.Event, limit: LimitOrder, type: string, liquidity: BigInt): void {
+    let action = new LimitOrderAction(event.transaction.hash.toHexString() + "-" + event.logIndex.toString());
+    action.limitOrder = limit.id;
+    action.type = type;
+    action.owner = limit.owner;
+    action.pool = limit.pool;
+    action.epoch = limit.epoch;
+    action.liquidity = liquidity;
+    action.initialLiquidity = limit.initialLiquidity;
+    action.killedLiquidity = limit.killedLiquidity;
+    action.remainingLiquidity = limit.liquidity;
+    action.timestamp = event.block.timestamp;
+    action.transactionHash = event.transaction.hash;
+    action.save();
+}
+
+function createEpochAction(event: ethereum.Event, epoch: Epoch, type: string, tickLower: i32, zeroToOne: boolean): void {
+    let action = new EpochAction(event.transaction.hash.toHexString() + "-" + event.logIndex.toString());
+    action.epoch = epoch.id;
+    action.type = type;
+    action.pool = epoch.pool;
+    action.tickLower = BigInt.fromI32(tickLower);
+    action.zeroToOne = zeroToOne;
+    action.totalLiquidity = epoch.totalLiquidity;
+    action.timestamp = event.block.timestamp;
+    action.transactionHash = event.transaction.hash;
+    action.save();
+}
 
 export function PlaceHandler(event: Place): void{
     let epoch = Epoch.load(event.params.epoch.toString());
@@ -41,6 +70,7 @@ export function PlaceHandler(event: Place): void{
     limit.killed = false;
     limit.save();
 
+    createLimitOrderAction(event, limit, "Place", event.params.liquidity);
 }
 
 export function FillHandler(event: Fill): void{
@@ -49,6 +79,8 @@ export function FillHandler(event: Fill): void{
         epoch.filled = true;
         epoch.fillTimestamp = event.block.timestamp;
         epoch.save();
+
+        createEpochAction(event, epoch, "Fill", event.params.tickLower, event.params.zeroForOne);
     }
 }
 
@@ -66,8 +98,10 @@ export function KillHandler(event: Kill): void{
         if(limit.killedLiquidity == limit.initialLiquidity){
             limit.killed = true;
             limit.closeTimestamp = event.block.timestamp;
-        } 
+        }
         limit.save();
+
+        createLimitOrderAction(event, limit, "Kill", event.params.liquidity);
     }
 
 }
@@ -86,5 +120,7 @@ export function WithdrawHandler(event: Withdraw): void{
             limit.closeTimestamp = event.block.timestamp;
         }
         limit.save();
+
+        createLimitOrderAction(event, limit, "Withdraw", event.params.liquidity);
     }
 }
